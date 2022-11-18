f6a7e9cf9b650abfe37a3c96555d57b60ae20b6a
jliang249 <jiayi17liang@gmail.com>
2022-11-18 15:48:02 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,7 +1,7 @@
 # Create your grading script here
 
 rm -rf student-submission
-git clone $1 student-submission
+git clone --quiet $1 student-submission
 # echo 'Finished cloning'
 
 cp TestListExamples.java student-submission
