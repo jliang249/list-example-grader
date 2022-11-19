3f4a5bfab78480b11897f59792072c89d1241642
jliang249 <jiayi17liang@gmail.com>
2022-11-18 16:00:55 -0800

    update

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,10 +1,55 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+class SomeStringChecker implements StringChecker {
+
+  @Override
+  public boolean checkString(String s) {
+    if (s.indexOf('a') == 0 || s.indexOf('e') == 0 || s.indexOf('i') == 0 || s.indexOf('o') == 0
+        || s.indexOf('u') == 0)
+      return true;
+    return false;
+  }
+
+}
 
 public class TestListExamples {
-  @Test(timeout = 100)
-  public void testTimeout() {
-    while(true) {}
-  }
   // Write your grading tests here!
+  @Test
+  public void testFilter() {
+    List<String> input = Arrays.asList("apple", "or");
+    List<String> output = Arrays.asList("apple", "or");
+    SomeStringChecker stringChecker = new SomeStringChecker();
+    assertEquals(output, ListExamples.filter(input,
+        stringChecker));
+  }
+
+  @Test
+  public void testFilter2() {
+    List<String> input = Arrays.asList();
+    List<String> output = Arrays.asList();
+    SomeStringChecker stringChecker = new SomeStringChecker();
+    assertEquals(output,
+        ListExamples.filter(input, stringChecker));
+  }
+
+  @Test
+  public void testFilter3() {
+    List<String> input = Arrays.asList("banana");
+    List<String> output = Arrays.asList();
+    SomeStringChecker stringChecker = new SomeStringChecker();
+    assertEquals(output, ListExamples.filter(input,
+        stringChecker));
+  }
+
+  @Test
+  public void testMerge3() {
+    List<String> list1 = Arrays.asList("apple", "banana", "cat");
+    List<String> list2 = Arrays.asList("banana");
+    List<String> output = Arrays.asList("apple", "banana", "banana", "cat");
+    assertEquals(output, ListExamples.merge(list1, list2));
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,32 +1,38 @@
 # Create your grading script here
 
+set -e
 rm -rf student-submission
-git clone --quiet $1 student-submission
-# echo 'Finished cloning'
+git clone $1 student-submission
 
-cp TestListExamples.java student-submission
-cp ParseOutput.java student-submission
+CP = ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+
+#check if file exist 
+if[[ -f "./student-submission/ListExamples.java"]]
+then 
+    echo "Right file submitted!"
+
+else 
+    echo "Wrong file submitted!"
+
+fi 
+
+cp TestListExamples.java ./student-submission/
+cp -r lib ./student-submission/
+cp ParseOutput.java ./student-submission/
+set +e 
+
 cd student-submission
+javac -cp $CP *.java
+java -cp $CP org.junt.runner.JUnitCore TestListExamples 
 
-if[[!(-e ListExamples.java)]]{
-    echo "No ListeExamples.java found! Wrong file name!"
-    exit 1
-}
-
-fi
-
-javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar.java
-
-if[[!($? == 0)]]{
+if [[!($? == 0)]]
+then 
     echo "File failed to compile"
-    exit 1
-}
+    exit 
 
 fi
 
-java ParseOutput $(java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples)
-
-echo "Score was $?" 
-
+java ParseOutput $(java $CP org.junit.runner.JUnitCore TestListExamples)
+echo "Score was $?"
 
 
