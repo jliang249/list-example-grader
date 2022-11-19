b453290dcbaaf342f7da59a837f6b7268856af22
jliang249 <jiayi17liang@gmail.com>
2022-11-18 16:13:44 -0800

    update

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,55 +1,31 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.*;
-
-class SomeStringChecker implements StringChecker {
-
-  @Override
-  public boolean checkString(String s) {
-    if (s.indexOf('a') == 0 || s.indexOf('e') == 0 || s.indexOf('i') == 0 || s.indexOf('o') == 0
-        || s.indexOf('u') == 0)
-      return true;
-    return false;
-  }
-
-}
 
 public class TestListExamples {
   // Write your grading tests here!
-  @Test
-  public void testFilter() {
-    List<String> input = Arrays.asList("apple", "or");
-    List<String> output = Arrays.asList("apple", "or");
-    SomeStringChecker stringChecker = new SomeStringChecker();
-    assertEquals(output, ListExamples.filter(input,
-        stringChecker));
-  }
+
+    public class NewChecker implements StringChecker{
+        public boolean checkString(String S){
+            return S.length() > 4;
+        }
+    }
+
 
   @Test
-  public void testFilter2() {
-    List<String> input = Arrays.asList();
-    List<String> output = Arrays.asList();
-    SomeStringChecker stringChecker = new SomeStringChecker();
-    assertEquals(output,
-        ListExamples.filter(input, stringChecker));
-  }
+    public void testFilter(){
+        List<String> input = List.of("irony", "smile");
+        StringChecker checker = new NewChecker();
 
-  @Test
-  public void testFilter3() {
-    List<String> input = Arrays.asList("banana");
-    List<String> output = Arrays.asList();
-    SomeStringChecker stringChecker = new SomeStringChecker();
-    assertEquals(output, ListExamples.filter(input,
-        stringChecker));
-  }
+        assertArrayEquals(input.toArray(), ListExamples.filter(input, checker).toArray());
+    }    
 
-  @Test
-  public void testMerge3() {
-    List<String> list1 = Arrays.asList("apple", "banana", "cat");
-    List<String> list2 = Arrays.asList("banana");
-    List<String> output = Arrays.asList("apple", "banana", "banana", "cat");
-    assertEquals(output, ListExamples.merge(list1, list2));
-  }
+    @Test
+    public void testMerge(){
+        List<String> input1 = List.of("apple", "banana");
+        List<String> input2 = List.of("aardvark", "chili", "dog");
+        List<String> expected = List.of("aardvark", "apple", "banana", "chili", "dog");
+
+        assertArrayEquals(expected.toArray(), ListExamples.merge(input1, input2).toArray());
+    }
 }
--- grade.sh
+++ grade.sh
@@ -9,30 +9,25 @@ CP = ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 #check if file exist 
 if [-e ListExamples.java]
     then 
-    echo "Right file submitted!"
+        echo "Right file submitted!"
 
     else 
-    echo "Wrong file submitted!"
-
+        echo "Wrong file submitted!"
 fi 
 
 cp TestListExamples.java student-submission
-cp -r lib student-submission
 cp ParseOutput.java student-submission
-set +e 
+cd student submission 
 
-cd student-submission
 javac -cp $CP *.java
 java -cp $CP org.junt.runner.JUnitCore TestListExamples 
 
 if [[!($? == 0)]]
     then 
-    echo "File failed to compile"
-    exit 
-
+        echo "File failed to compile"
+        exit 
 fi
 
-java ParseOutput $(java $CP org.junit.runner.JUnitCore TestListExamples)
 echo "Score was $?"
 
 
