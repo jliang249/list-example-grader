041762acec0e6bfd511b979f023eaec97c606a18
jliang249 <jiayi17liang@gmail.com>
2022-11-28 00:51:46 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,33 +1,45 @@
 # Create your grading script here
-
 set -e
+CP = ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 rm -rf student-submission
 git clone $1 student-submission
 
 #check if file exist 
-if [-e ListExamples.java]
-    then 
+if [[-f "./student-submission/ListExamples.java"]]
+    then                                                                                                 Top
         echo "Right file submitted!"
 
-    else 
+    else
         echo "Wrong file submitted!"
-fi 
-
-cp TestListExamples.java student-submission
-cp ParseOutput.java student-submission
-cd student submission 
-
-CP = ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
-
-javac -cp $CP *.java
-java -cp $CP org.junt.runner.JUnitCore TestListExamples 
-
-if [[!($? == 0)]]
-    then 
-        echo "File failed to compile"
-        exit 
 fi
 
-echo "Score was $?"
+cp TestListExamples.java ./student-submission/ > line17_out 2> line17_err
+cp -r lib ./student-submission/ > line18_out 2> line18_err
+set +e
+cd student submission > line20_out 2> line20_err
 
+
+javac -cp $CP *.java > line23_out 2> line23_err
+java -cp $CP org.junit.runner.JUnitCore TestListExamples 1> stdout 2> stderr
+
+CHECK = $(grep -o "Failures" stdout)
+P = $(grep -E -o ".{0,2}test.{0,0}" stdout)
+F = $(grep -E -o "Failures.{0,3}" stdout)
+
+if [[$CHECK]]
+        then
+                echo "Total: 4 tests"
+                grep "F" stdout
+        else
+                echo "Total: 4 tests" 
+                echo "Test pass: $P"
+
+fi
+echo $?
+if [[$? - eq 0]]
+        then
+                exit
+        else
+                echo "Test failed!"
+fi
 
